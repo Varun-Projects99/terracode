c4737ed09ac48f0c6e091fbf636dae318f20fb4b
Varun <varunb1090@gmail.com>
2026-07-02 14:47:45 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "DevOpsRG"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  default = "varunstorage9997"
+}
